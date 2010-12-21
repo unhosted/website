94f40baad96ed7fc8c8e13870524d5089f24474c
Michiel@unhosted <michiel@unhosted.org>
2010-12-21 21:51:45 +0800

    removing reference to Tonido, because apparently jailbreaking them voids the warranty. also committing webfinger for @unhosted.org users (me;)

---
+++ html/webfinger.php
@@ -0,0 +1,14 @@
+<?php
+if($_GET['uri']=='michiel@unhosted.org' || $_GET['id']=='acct:michiel@unhosted.org') {
+?>
+<?xml version='1.0' encoding='UTF-8'>
+<XRD xmlns='http://docs.oasis-open.org/ns/xri/xrd-1.0'>
+
+  <Alias>http://unhosted.org/users/michiel</Alias>
+
+  <Link rel='http://microformats.org/profile/hcard'
+        href='http://unhosted.org/users/michiel' />
+  <!-- Include additional Link elements here. --> 
+</XRD>
+<?php
+}
