01229730bf849c2583890f4fb29f4ecd72bcac2d
Michiel@unhosted <michiel@unhosted.org>
2011-09-29 11:54:50 +0200

    adding rmll slides for reference

---
+++ rmll/_/js/functions.js
@@ -0,0 +1,14 @@
+// remap jQuery to $
+(function($){
+	$('.scrollPage').click(function() {
+	   var elementClicked = $(this).attr("href");
+	   var destination = $(elementClicked).offset().top;
+	   $("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination-20}, 500 );
+	 
+	   return false;
+	});
+ 
+
+ 
+
+})(window.jQuery);
