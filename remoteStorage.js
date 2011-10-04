e110bd4ccf2fbb488a23ca0926b110acf74743b4
Michiel@unhosted <michiel@unhosted.org>
2011-10-04 21:25:49 +0200

    improving script, adding button to two more apps

--- remoteStorage.js
+++ remoteStorage.js
@@ -1,25 +1,45 @@
 (function() {
   if(!window.remoteStorage) {
-    window.remoteStorage = {
-      getItem: function(k) {
-        return localStorage.getItem(k);
-      },
-      setItem: function(k,v) {
-        var ret = localStorage.setItem(k, v);
-        window.remoteStorage.length = localStorage.length;
-        return ret;
-      },
-      removeItem: function(k,v) {
-        var ret = localStorage.removeItem(k, v);
-        window.remoteStorage.length = localStorage.length;
-        return ret;
-      },
-      clear: function() {
-        return localStorage.clear();
-      },
-      setUserAddress: function(userAddress) {
-        alert('fake setting user address "'+userAddress+'"');
+    window.remoteStorage = (function(){
+      function pushAction(action) {
+        var oldQueue = localStorage.getItem('_remoteStorageActionQueue');
+        if(oldQueue==null){
+          oldQueue=[];
+        }
+        localStorage.setItem('_remoteStorageActionQueue', oldQueue.push(action));
       }
-    }
+      return {
+        length: localStorage.length,
+        userAddress: 'user@island',
+        connected: false,
+        working: false,     
+        getItem: function(k) {
+          return localStorage.getItem('_remoteStorage_'+k);
+        },
+        setItem: function(k,v) {
+          pushAction({action: 'setItem', key: k, value: v});
+          var ret = localStorage.setItem('_remoteStorage_'+k, v);
+          this.length = localStorage.length;
+          return ret;
+        },
+        removeItem: function(k) {
+          pushAction({action: 'removeItem', key: k});
+          var ret = localStorage.removeItem('_remoteStorage_'+k);
+          window.remoteStorage.length = localStorage.length;
+          return ret;
+        },
+        clear: function() {
+          localStorage.setItem('_remoteStorageActionQueue', '[{"action": "clear"}]');
+          for(var i=0;i<localStorage.length;i++) {
+            if(localStorage.key(i).substring(0,15)=='_remoteStorage_') {
+              localStorage.removeItem(localStorage.key(i));
+            }
+          }
+        },
+        setUserAddress: function(userAddress) {
+          alert('fake setting user address "'+userAddress+'"');
+        }
+      }
+    })()
   }
 })()
