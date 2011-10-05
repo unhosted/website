4871534b358048febc6437d80621b7038253e94e
Michiel@unhosted <michiel@unhosted.org>
2011-10-05 18:41:32 +0200

    JSON stringify index before setting it

--- remoteStorage.js
+++ remoteStorage.js
@@ -52,6 +52,146 @@
     }
 
 
+      //////////////////////////////
+     // base64 (from webtoolkit) //
+    //////////////////////////////
+     
+    var Base64 = {
+     
+      // private property
+      _keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+     
+      // public method for encoding
+      encode : function (input) {
+	var output = "";
+	var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+	var i = 0;
+     
+	input = Base64._utf8_encode(input);
+     
+	while (i < input.length) {
+     
+	  chr1 = input.charCodeAt(i++);
+	  chr2 = input.charCodeAt(i++);
+	  chr3 = input.charCodeAt(i++);
+     
+	  enc1 = chr1 >> 2;
+	  enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+	  enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+	  enc4 = chr3 & 63;
+     
+	  if (isNaN(chr2)) {
+	    enc3 = enc4 = 64;
+	  } else if (isNaN(chr3)) {
+	    enc4 = 64;
+	  }
+     
+	  output = output +
+	  this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
+	  this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
+     
+	}
+     
+	return output;
+      },
+     
+      // public method for decoding
+      decode : function (input) {
+	var output = "";
+	var chr1, chr2, chr3;
+	var enc1, enc2, enc3, enc4;
+	var i = 0;
+     
+	input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+     
+	while (i < input.length) {
+     
+	  enc1 = this._keyStr.indexOf(input.charAt(i++));
+	  enc2 = this._keyStr.indexOf(input.charAt(i++));
+	  enc3 = this._keyStr.indexOf(input.charAt(i++));
+	  enc4 = this._keyStr.indexOf(input.charAt(i++));
+     
+	  chr1 = (enc1 << 2) | (enc2 >> 4);
+	  chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+	  chr3 = ((enc3 & 3) << 6) | enc4;
+     
+	  output = output + String.fromCharCode(chr1);
+     
+	  if (enc3 != 64) {
+	    output = output + String.fromCharCode(chr2);
+	  }
+	  if (enc4 != 64) {
+	    output = output + String.fromCharCode(chr3);
+	  }
+     
+	}
+     
+	output = Base64._utf8_decode(output);
+     
+	return output;
+     
+      },
+     
+      // private method for UTF-8 encoding
+      _utf8_encode : function (string) {
+	string = string.replace(/\r\n/g,"\n");
+	var utftext = "";
+     
+	for (var n = 0; n < string.length; n++) {
+     
+	  var c = string.charCodeAt(n);
+     
+	  if (c < 128) {
+	    utftext += String.fromCharCode(c);
+	  }
+	  else if((c > 127) && (c < 2048)) {
+	    utftext += String.fromCharCode((c >> 6) | 192);
+	    utftext += String.fromCharCode((c & 63) | 128);
+	  }
+	  else {
+	    utftext += String.fromCharCode((c >> 12) | 224);
+	    utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+	    utftext += String.fromCharCode((c & 63) | 128);
+	  }
+     
+	}
+     
+	return utftext;
+      },
+     
+      // private method for UTF-8 decoding
+      _utf8_decode : function (utftext) {
+	var string = "";
+	var i = 0;
+	var c = c1 = c2 = 0;
+     
+	while ( i < utftext.length ) {
+     
+	  c = utftext.charCodeAt(i);
+     
+	  if (c < 128) {
+	    string += String.fromCharCode(c);
+	    i++;
+	  }
+	  else if((c > 191) && (c < 224)) {
+	    c2 = utftext.charCodeAt(i+1);
+	    string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+	    i += 2;
+	  }
+	  else {
+	    c2 = utftext.charCodeAt(i+1);
+	    c3 = utftext.charCodeAt(i+2);
+	    string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+	    i += 3;
+	  }
+     
+	}
+     
+	return string;
+      }
+     
+    }
+
       ///////////////
      // Webfinger //
     ///////////////
@@ -292,7 +432,7 @@
 	      cb(obj);
 	    } catch(e){
 	      cb({
-		success:true
+	  success:true
 	      });
 	    }
 	  },
@@ -315,16 +455,22 @@
           index={
             '_remoteStorageAll': revision
           }
-          localStorage.setItem('_remoteStorageIndex', index);
+          localStorage.setItem('_remoteStorageIndex', JSON.stringify(index));
           doCall('PUT', '_remoteStorageIndex', JSON.stringify(index), revision, cb);
         },
         setItem: function(key, value, revision, cb) {
           var index = JSON.parse(localStorage.getItem('_remoteStorageIndex'));
+          if(!index) {//first use
+            index={
+              '_remoteStorageAll': 0
+            };
+            localStorage.setItem('_remoteStorageIndex', JSON.stringify(index));
+          }
           if(index['_remoteStorageAll']<revision) {
             doCall('PUT', key, value, revision, function() {
               index[key]=revision;
               index['_remoteStorageAll']=revision;
-              localStorage.setItem('_remoteStorageIndex', index);
+              localStorage.setItem('_remoteStorageIndex', JSON.stringify(index));
               doCall('PUT', '_remoteStorageIndex', JSON.stringify(index), revision, cb);
             });
           }
@@ -335,7 +481,7 @@
             doCall('DELETE', key, null, revision, function() {
               index[key]=undefined;
               index['_remoteStorageAll']=revision;
-              localStorage.setItem('_remoteStorageIndex', index);
+              localStorage.setItem('_remoteStorageIndex', JSON.stringify(index));
               doCall('PUT', '_remoteStorageIndex', JSON.stringify(index), revision, cb);
             });
           }
@@ -361,7 +507,7 @@
                   localStorage.setItem(data.key, data.value);
                   var localIndex = localStorage.getItem('_remoteStorageItem');
                   localIndex[data.key]=data._revision;
-                  localStorage.setItem('_remoteStorageIndex', localIndex);              
+                  localStorage.setItem('_remoteStorageIndex', JSON.stringify(localIndex));              
                 });
               }
             }
@@ -402,16 +548,15 @@
         queue.push(action);
         localStorage.setItem('_remoteStorageActionQueue', JSON.stringify(queue));
       }
+      work(0);
       return {
         length: localStorage.length,
-        userAddress: 'user@island',
-        connected: false,
         getItem: function(k) {
           return localStorage.getItem('_remoteStorage_'+k);
         },
         setItem: function(k,v) {
-          pushAction({action: 'setItem', key: k, value: v}, this.connected);
-          if(this.connected) {
+          pushAction({action: 'setItem', key: k, value: v});
+          if(this.getBackend()) {
             work();
           }
           var ret = localStorage.setItem('_remoteStorage_'+k, v);
@@ -420,7 +565,7 @@
         },
         removeItem: function(k) {
           pushAction({action: 'removeItem', key: k});
-          if(this.connected) {
+          if(this.getBackend()) {
             work();
           }
           var ret = localStorage.removeItem('_remoteStorage_'+k);
@@ -429,7 +574,7 @@
         },
         clear: function() {
           localStorage.setItem('_remoteStorageActionQueue', '[{"action": "clear"}]');
-          if(this.connected) {
+          if(this.getBackend()) {
             work();
           }
           for(var i=0;i<localStorage.length;i++) {
@@ -440,8 +585,6 @@
         },
         setBackend: function(userAddress) {
           backend.connect(userAddress, function() {
-            this.userAddress = userAddress;
-            this.connected = true;
             work();
           })
         },
