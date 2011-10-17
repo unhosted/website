701262889b6bf3c225fe041823bbc0cf0b2cb343
Michiel de Jong <michiel@unhosted.org>
2011-10-17 12:28:17 +0000

    adding a statics server

---
+++ server.js
@@ -0,0 +1,15 @@
+var static = require('node-static');
+
+//
+// Create a node-static server instance to serve the './public' folder
+//
+var file = new(static.Server)('.');
+
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        //
+        // Serve files!
+        //
+        file.serve(request, response);
+    });
+}).listen(8080);
