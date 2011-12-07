faacc56a31e2fda8bc4fbfe965e601fdaf305b40
Jan-Christoph Borchardt <jan@unhosted.org>
2011-12-07 02:21:05 +0100

    removing all this legacy stuff

--- remoteStorage.js
+++
@@ -1,6 +0,0 @@
-the file you are looking for is here:
-https://github.com/unhosted/remoteStorage.js
-
-unhosted.org is hosted on nodejitsu, with a http server that is here:
-https://github.com/unhosted/experiments/tree/master/nodejitsu/unhosted
-and that merges the 'website' repo and the 'remoteStorage.js' repo into one, depending on which file path you request.
