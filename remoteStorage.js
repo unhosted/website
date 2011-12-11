ca65b8ed589ac958e8b4345d78a7ab5fdeee5f99
martin <martin@siarp.de>
2011-12-12 00:56:09 +0100

    add remoteStorage.js bootstrap scripts

---
+++ remoteStorage.js
@@ -0,0 +1,6 @@
+(function(){
+  var s = document.createElement('script');
+  s.setAttribute('src', 'http://unhosted.org/require.js');
+  s.setAttribute('data-main', 'http://unhosted.nodejitsu.com/main');
+  document.head.appendChild(s);
+})();
