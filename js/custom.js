0a6448b9101b84b6b3ec3717cb165bdc6d5544a0
Jan-Christoph Borchardt <jan@unhosted.org>
2012-02-13 01:52:49 +0200

    added new manifesto, some possibly breaking changes to navigation

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,25 @@
-// TODO: also change web address and check for web address in the first place
-
 function navSwitchActive(navEntry) {
   $('.nav .active').removeClass('active');
   $(navEntry).parent().addClass('active');
+  $('#content').addClass(navEntry.slice(1));
+  window.location.pathname = navEntry.slice(1);
 }
 // can these calls be simplified into one function?
-$('#nav-introduction').click(function() { $('#content').load('introduction.html'); navSwitchActive($(this)); });
-$('#nav-manifesto').click(function() { $('#content').load('manifesto.html'); navSwitchActive($(this)); });
-$('#nav-developer').click(function() { $('#content').load('developer.html'); navSwitchActive($(this)); });
-$('#nav-community').click(function() { $('#content').load('community.html'); navSwitchActive($(this)); });
-$('#nav-thanks').click(function() { $('#content').load('thanks.html'); navSwitchActive($(this)); });
+$('#introduction').click(function() { $('#content').load('introduction.html'); navSwitchActive($(this)); });
+$('#manifesto').click(function() { $('#content').load('manifesto.html'); navSwitchActive($(this)); });
+$('#developer').click(function() { $('#content').load('developer.html'); navSwitchActive($(this)); });
+$('#community').click(function() { $('#content').load('community.html'); navSwitchActive($(this)); });
+$('#thanks').click(function() { $('#content').load('thanks.html'); navSwitchActive($(this)); });
+
+$(document).ready(function() {
+  if(!$('#content').hasClass(window.location.pathname)) {
+    var path = window.location.pathname;
+    if(!(path=='introduction' OR path=='manifesto' OR path=='developer' OR path=='community' OR path=='thanks')) {
+      $('#content').load(path+'.html');
+      navSwitchActive('#'+path);
+    } else {
+      $('#content').load('introduction.html');
+      navSwitchActive('#introduction');
+    }
+  }
+});
