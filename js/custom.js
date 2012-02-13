0561fd6ddb99b17cc9dd7f8e7591e752cba19cbc
Jan-Christoph Borchardt <jan@unhosted.org>
2012-02-13 17:52:20 +0200

    hopefully it works now, hopefully

--- js/custom.js
+++ js/custom.js
@@ -1,25 +1,26 @@
-function navSwitchActive(navEntry) {
-  $('.nav .active').removeClass('active');
-  $(navEntry).parent().addClass('active');
-  $('#content').addClass(navEntry.slice(1));
-  window.location.pathname = navEntry.slice(1);
+function loadpage(page) {
+	$('.nav .active').removeClass('active');
+	$('#nav-'+page).parent().addClass('active');
+	$('#content').removeClass(window.location.hash.slice(1));
+	$('#content').addClass(page);
+	window.location.hash = '#'+page;
+	$('#content').load(page+'.html');
 }
+
 // can these calls be simplified into one function?
-$('#introduction').click(function() { $('#content').load('introduction.html'); navSwitchActive($(this)); });
-$('#manifesto').click(function() { $('#content').load('manifesto.html'); navSwitchActive($(this)); });
-$('#developer').click(function() { $('#content').load('developer.html'); navSwitchActive($(this)); });
-$('#community').click(function() { $('#content').load('community.html'); navSwitchActive($(this)); });
-$('#thanks').click(function() { $('#content').load('thanks.html'); navSwitchActive($(this)); });
+$('#nav-introduction').click(function() { loadpage('introduction'); });
+$('#nav-manifesto').click(function() { loadpage('manifesto'); });
+$('#nav-developer').click(function() { loadpage('developer'); });
+$('#nav-community').click(function() { loadpage('community'); });
+$('#nav-thanks').click(function() { loadpage('thanks'); });
 
 $(document).ready(function() {
-  if(!$('#content').hasClass(window.location.pathname)) {
-    var path = window.location.pathname;
-    if(!(path=='introduction' OR path=='manifesto' OR path=='developer' OR path=='community' OR path=='thanks')) {
-      $('#content').load(path+'.html');
-      navSwitchActive('#'+path);
-    } else {
-      $('#content').load('introduction.html');
-      navSwitchActive('#introduction');
-    }
-  }
+	if(!$('.active').hasClass(window.location.hash.slice(1))) {
+		var page = window.location.hash.slice(1);
+		if(page=='introduction' || page=='manifesto' || page=='developer' || page=='community' || page=='thanks') {
+			loadpage(page);
+		} else {
+			loadpage('introduction');
+		}
+	}
 });
