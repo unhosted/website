edb7c62af4f1bae247192a3e65346d362ce7553f
Michiel de Jong <michiel@unhosted.org>
2012-03-15 05:50:55 +0000

    update developer page -> 'remoteStorage'

--- js/custom.js
+++ js/custom.js
@@ -11,16 +11,18 @@ function loadpage(page) {
 // can these calls be simplified into one function?
 $('#nav-introduction').click(function() { loadpage('introduction'); });
 $('#nav-manifesto').click(function() { loadpage('manifesto'); });
-$('#nav-developer').click(function() { loadpage('developer'); });
+$('#nav-remotestorage').click(function() { loadpage('remotestorage'); });
 $('#nav-community').click(function() { loadpage('community'); });
 $('#nav-thanks').click(function() { loadpage('thanks'); });
 
 $(document).ready(function() {
 	if(!$('.active').hasClass(window.location.hash.slice(1))) {
 		var page = window.location.hash.slice(1);
-		if(page=='introduction' || page=='manifesto' || page=='developer' || page=='community' || page=='thanks') {
+		if(page=='introduction' || page=='manifesto' || page=='remotestorage' || page=='community' || page=='thanks') {
 			loadpage(page);
-		} else {
+		} else if(page=='developer') {
+      loadpage('remotestorage');
+    } else {
 			loadpage('introduction');
 		}
 	}
