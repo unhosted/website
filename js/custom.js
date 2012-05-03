a684d66b7341784520fc4cc7d22c126730680435
Michiel de Jong <michiel@unhosted.org>
2012-05-03 12:40:34 +0200

    make remotestorage the default page

--- js/custom.js
+++ js/custom.js
@@ -15,15 +15,13 @@ $('#nav-remotestorage').click(function() { loadpage('remotestorage'); });
 $('#nav-community').click(function() { loadpage('community'); });
 $('#nav-thanks').click(function() { loadpage('thanks'); });
 
-$(document).ready(function() {
+$(document).ready(function() 
 	if(!$('.active').hasClass(window.location.hash.slice(1))) {
 		var page = window.location.hash.slice(1);
 		if(page=='introduction' || page=='manifesto' || page=='remotestorage' || page=='community' || page=='thanks') {
 			loadpage(page);
-		} else if(page=='developer') {
-      loadpage('remotestorage');
-    } else {
-			loadpage('introduction');
+		} else {
+			loadpage('remotestorage');
 		}
 	}
 	prettyPrint();
