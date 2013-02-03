f6e6273d05169d01a3f114f8d05051b90b7a5a63
Michiel de Jong <michiel@unhosted.org>
2013-02-03 16:42:04 +0800

    debug adventures build script

--- adventures/build/build.js
+++ adventures/build/build.js
@@ -5,26 +5,25 @@ var fs = require('fs'),
   part1 = fs.readFileSync('build/1.html'),
   part2 = fs.readFileSync('build/2.html'),
   part3 = fs.readFileSync('build/3.html'),
-  part4 = fs.readFileSync('build/4.html'),
-  episodes = fs.readFileSync('build/episodes.js');
+  episodes = JSON.parse(fs.readFileSync('build/episodes.js'));
 
 function getFilename(i) {
   return i+'/'+episodes[i].split(' ').join('-')+'.html';
 }
 function makeEpisodesDiv(current) {
-  var str = '<ul>\n';
+  var str = '';
   for(var i in episodes) {
     if(i == current) {
-      str += '  <li> '+i+'. <strong>'+episodes[i]+'</strong></li>\n';
+      str += '        <p><strong>'+ i +'. '+ episodes[i] +'</strong></p>\n';
     } else {
-      str += '  <li> '+i+'. <a href="../'+getFilename(i)'">'+episodes[i]+'</a></li>\n';
+      str += '        <p> '+ i +'. <a href="../'+ getFilename(i) +'">'+ episodes[i] +'</a></p>\n';
     }
   }
-  return str+'</ul>\n';
+  return str;
 }
 for(var i=1; i<=latestEpisode; i++) {
   var source = fs.readFileSync(i+'/source.html'),
-    title = '      <title>Unhosted '+ i +': '+ episodes[i] +'</title>\n',
-    header = '      <h2>'+ i + '. '+ episodes[i] +'</h2>\n';
-  fs.writeFileSync(getFilename(i), part1 + title + part2 + header + source + part3 + makeEpisodeDiv(i) + part4);
+    title = '    <title>Unhosted '+ i +': '+ episodes[i] +'</title>\n',
+    header = '      <h2>'+ i + '. '+ episodes[i] +'</h2>\n\n';
+  fs.writeFileSync(getFilename(i), part0 + title + part1 + header + source + part2 + makeEpisodesDiv(i) + part3);
 }
