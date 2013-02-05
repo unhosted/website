b32f9f7ee44af11ea31b6407d739919ef38d8439
Michiel de Jong <michiel@unhosted.org>
2013-02-05 13:11:24 +0800

    generate feed.rss (with ISO strings)

--- adventures/build/build.js
+++ adventures/build/build.js
@@ -1,4 +1,5 @@
-var latestEpisode = 8;
+var latestEpisode = 8,
+  zeroDate = 1355227200000;
 
 var fs = require('fs'),
   part0 = fs.readFileSync('build/0.html'),
@@ -47,3 +48,32 @@ function writeEpisodes() {
 //...
 processTitles();
 writeEpisodes();
+writeRss();
+function writeRss() {
+  var str = '<?xml version="1.0" encoding="UTF-8"?>\n'
+    +'<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:georss="http://www.georss.org/georss">\n'
+    +'  <channel>\n'
+    +'    <title>Unhosted Adventures</title>\n'
+    +'    <link>https://unhosted.org/adventures/</link>\n'
+    +'    <atom:link type="application/rss+xml" href="https://unhosted.org/adventures/feed.rss" rel="self"></atom:link>\n'
+    +'    <image>\n'
+    +'      <link>https://unhosted.org/adventures/img/island-color.jpg</link>\n'
+    +'      <title>Unhosted Adventures</title>\n'
+    +'      <url>https://unhosted.org/adventures/img/island-color.jpg</url>\n'
+    +'    </image>\n'
+    +'    <description>Official handbook of the No Cookie Crew</description>\n'
+    +'    <language>en-us</language>\n'
+    +'    <ttl>40</ttl>\n';
+  for(var i=latestEpisode; i>=1; i--) {
+    str += '    <item>\n'
+      +'      <title>'+i+'. '+episodes[i]+'</title>\n'
+      +'      <description>Episode '+i+' of Unhosted Adventures</description>\n'
+      +'      <pubDate>'+new Date(zeroDate+i*7*86400*1000).toISOString()+'</pubDate>\n'
+      +'      <guid>https://unhosted.org/adventures/'+getFilename(i)+'</guid>\n'
+      +'      <link>https://unhosted.org/adventures/'+getFilename(i)+'</link>\n'
+      +'      <georss:point>10.2 104.0</georss:point>\n'
+      +'    </item>\n';
+  }
+  str += ' </channel>\n</rss>\n';
+  fs.writeFileSync('feed.rss', str);
+}
