9bd72c2438a845aaefe223dea1f2c2356f87b05d
Michiel de Jong <michiel@unhosted.org>
2013-02-16 00:34:16 +0800

    redirect to https://unhosted.org/

--- remoteStorage.js
+++
@@ -1,6 +0,0 @@
-(function(){
-  var s = document.createElement('script');
-  s.setAttribute('src', 'http://unhosted.nodejitsu.com/require.js');
-  s.setAttribute('data-main', 'http://unhosted.nodejitsu.com/main');
-  document.head.appendChild(s);
-})();
